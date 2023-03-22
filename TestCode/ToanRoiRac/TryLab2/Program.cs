f54459fd9f62d02e16695b2ec3dbba63b4892544
Đoàn Quang Huy <quanghuybest@gmail.com>
2023-03-22 09:46:04 +0700

    Băt lỗi và tách hàm

--- TestCode/ToanRoiRac/TryLab2/Program.cs
+++ TestCode/ToanRoiRac/TryLab2/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Text;
 using System;
-using System.Security.Cryptography;
 using TryLab2;
 
 class Program
@@ -26,11 +25,8 @@ class Program
         Console.OutputEncoding = Encoding.UTF8;
         Console.ForegroundColor = ConsoleColor.Green;
         //Console.BackgroundColor = ConsoleColor.Yellow;
-        //int[] U = new int[0];
-        //int[] A = new int[0];
-        //int[] B = new int[0];
         Set set = new Set();
-
+        HamThuatToan htt = new HamThuatToan();
         while (true)
         {
             Console.WriteLine("================================ MENU ================================");
@@ -67,48 +63,59 @@ class Program
                 case Menu.NhapXtrongA:
                     // Nhập phần tử x
                     Console.Write("Nhập vào phần tử x để tìm trong tập A: ");
-                    int x = int.Parse(Console.ReadLine());
-                    set.Xuat();
-                    // Kiểm tra xem x có thuộc tập A hay không
-                    bool isInA = KiemTraCoTonTai(x, set._a);
-                    if (isInA)
+                    try
                     {
-                        Console.WriteLine($"Phần tử {x} thuộc tập A");
+                        int x = int.Parse(Console.ReadLine());
+                        set.Xuat();
+                        // Kiểm tra xem x có thuộc tập A hay không
+                        bool isInA = htt.KiemTraCoTonTai(x, set._a);
+                        if (isInA)
+                        {
+                            Console.WriteLine($"Phần tử {x} thuộc tập A");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Phần tử {x} không thuộc tập A");
+                        }
                     }
-                    else
+                    catch (Exception ex)
                     {
-                        Console.WriteLine($"Phần tử {x} không thuộc tập A");
+                        Console.WriteLine($"Phải là số! Lỗi >> {ex.Message}");
                     }
+
                     break;
                 case Menu.Giao:
                     // Tìm tập giao của A và B
-                    int[] TapGiao = Giao(set._a, set._b);
+                    int[] TapGiao = htt.Giao(set._a, set._b);
                     set.Xuat();
                     Console.WriteLine("Tập giao của A và B là: " + string.Join(" ", TapGiao));
                     break;
                 case Menu.Hop:
                     // Tìm tập hợp của A và B
-                    int[] TapHop = Hop(set._a, set._b);
+                    int[] TapHop = htt.Hop(set._a, set._b);
                     set.Xuat();
                     Console.WriteLine("Tập hợp của A và B là: " + string.Join(" ", TapHop));
                     break;
                 case Menu.Hieu:
                     // Tìm hiệu của A cho B
-                    int[] TapHieu = Hieu(set._a, set._b);
+                    int[] TapHieu = htt.Hieu(set._a, set._b);
                     set.Xuat();
                     Console.WriteLine("Hiệu của A cho B là: " + string.Join(" ", TapHieu));
                     break;
                 case Menu.Bu:
                     // Tìm phần bù của tập A trong tập U
-                    int[] TapBu = PhanBu(set._a, set._u);
+                    int[] TapBu = htt.PhanBu(set._a, set._u);
                     set.Xuat();
                     Console.WriteLine("Phần bù của tập A trong tập U là: " + string.Join(" ", TapBu));
                     break;
                 case Menu.HieuDoiXung:
                     // Tìm hiệu đối xứng của A và B
-                    int[] TapHieuDoiXung = HieuDoiXung(set._a, set._b, set._u);
+                    int[] TapHieuDoiXung = htt.HieuDoiXung(set._a, set._b, set._u);
                     set.Xuat();
                     Console.WriteLine("Hiệu đối xứng của A và B là: " + string.Join(" ", TapHieuDoiXung));
+                    break;
+                default:
+                    Console.WriteLine("Không có lựa chọn này! vui lòng thử lại.");
                     break;
             }
             Console.ForegroundColor = ConsoleColor.Red;
@@ -117,84 +124,6 @@ class Program
             Console.ReadKey();
             Console.Clear();
         }
-
     }
     // end Main
-  
-    // kiem tra xem x co nam trong tap A cho truoc khong
-    static bool KiemTraCoTonTai(int element, int[] set)
-    {
-        foreach (int x in set)
-        {
-            if (x == element)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-    static int[] Giao(int[] A, int[] B)
-    {
-        HashSet<int> setA = new HashSet<int>(A);
-        HashSet<int> setB = new HashSet<int>(B);
-
-        HashSet<int> DanhSachGiao = new HashSet<int>(setA);
-        DanhSachGiao.IntersectWith(setB);
-
-        return DanhSachGiao.ToArray();
-    }
-    // tính hợp của 2 tập A và B
-    static int[] Hop(int[] A, int[] B)
-    {
-        HashSet<int> hop = new HashSet<int>(A);
-        foreach (int i in B)
-        {
-            if (!hop.Contains(i))
-            {
-                hop.Add(i);
-            }
-        }
-        return hop.ToArray();
-    }
-    // Tính A hiệu B (A\B)
-    static int[] Hieu(int[] A, int[] B)
-    {
-        List<int> DanhSachHieu = new List<int>();
-        foreach (int x in A)
-        {
-            if (!KiemTraCoTonTai(x, B))
-            {
-                DanhSachHieu.Add(x);
-            }
-        }
-        return DanhSachHieu.ToArray();
-    }
-    // tinh phan bu
-    static int[] PhanBu(int[] A, int[] U)
-    {
-        List<int> DanhSachPhanBu = new List<int>();
-        foreach (int x in U)
-        {
-            if (!KiemTraCoTonTai(x, A))
-            {
-                DanhSachPhanBu.Add(x);
-            }
-        }
-        return DanhSachPhanBu.ToArray();
-    }
-    // tính hiệu đối xứng
-    static int[] HieuDoiXung(int[] A, int[] B, int[] U)
-    {
-        List<int> DanhSachHieu = new List<int>();
-
-        foreach (int x in U)
-        {
-            if (KiemTraCoTonTai(x, A) ^ KiemTraCoTonTai(x, B))
-            {
-                DanhSachHieu.Add(x);
-            }
-        }
-
-        return DanhSachHieu.ToArray();
-    }
 }
